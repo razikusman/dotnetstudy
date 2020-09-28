42f1fa7be2a9ff44db45bc0c9485e84affc2cc05
razikusman <razikusman@gmail.com>
2020-09-28 21:48:31 +0530

    Crud operation

---
+++ Wetherforecast/Wetherforecast/Models/Publisher.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wetherforecast.Models
+{
+    public class Publisher
+    {
+        public int PublisherId { get; set; }
+        public String PublsherName { get; set; }
+    }
+}
